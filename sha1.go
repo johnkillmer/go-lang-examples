eb7ad257c51e6f443d6c36e60a9126e42fea74ed
John Killmer <killmer.john@gmail.com>
2018-12-04 22:55:05 -0500

    Adding examples for base64 encoding

--- sha1.go
+++ sha1.go
@@ -11,13 +11,13 @@ func main() {
 	h := sha1.New()
 	h.Write([]byte(s))
 	bs := h.Sum(nil)
-	
+
 	h2 := md5.New()
 	h2.Write([]byte(s))
 	bs2 := h2.Sum(nil)
 
 	fmt.Println(s)
-    
-    fmt.Printf("Sha1: %x\n", bs)
-    fmt.Printf("MD5: %x\n", bs2)
+
+	fmt.Printf("Sha1: %x\n", bs)
+	fmt.Printf("MD5: %x\n", bs2)
 }
