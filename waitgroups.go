9b4c9af24b1a0608f67816337deb09bb51e9f9db
John Killmer <killmer.john@gmail.com>
2020-03-13 06:52:59 -0400

    Adding a rate limiting example

--- waitgroups.go
+++ waitgroups.go
@@ -7,6 +7,7 @@ import (
 )
 
 func worker(id int, wg *sync.WaitGroup) {
+	
 	defer wg.Done()
 
 	fmt.Printf("Worker %d starting\n", id)
@@ -18,8 +19,8 @@ func main() {
 	var wg sync.WaitGroup
 
 	for i := 1; i <= 5; i++ {
-		wg.Add(1)
-		go worker(i, &wg)
+	   wg.Add(1)
+	   go worker(i, &wg)
 	}
 
 	wg.Wait()
