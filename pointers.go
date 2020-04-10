13236e14096f29c46bba1464f151bc2b25b5b28a
John Killmer <killmer.john@gmail.com>
2020-04-10 08:17:16 -0400

    Adding an example for pointers

--- pointers.go
+++ pointers.go
@@ -4,7 +4,24 @@ import (
 	"fmt"
 )
 
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
 func main() {
-	i := 42
-	fmt.Println(i)
+	i := 1
+	fmt.Println("initial: ", i)
+
+	zeroval(i)
+	fmt.Println("zeroval: ", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroptr: ", i)
+
+	fmt.Println("pointer", &i)
+
 }
