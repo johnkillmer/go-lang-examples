b6f993c6f6e339dbb4709984cccc71c9648dd351
John Killmer <killmer.john@gmail.com>
2020-04-15 07:43:36 -0400

    formatting the code

--- methods.go
+++ methods.go
@@ -8,26 +8,26 @@ type rect struct {
 
 //pointer receiver - use when wanting to avoid a copy or when you want to mutate the receiving
 func (r *rect) area() int {
-   r.x=5;
-   return r.width * r.height
+	r.x = 5
+	return r.width * r.height
 }
 
-//value receiver 
+//value receiver
 func (r rect) perim() int {
-   r.x=2
-   return 2 * r.width + 2 * r.height
+	r.x = 2
+	return 2*r.width + 2*r.height
 }
 
 func main() {
-   r := rect{width:10, height:5}
-   fmt.Println(r)	
+	r := rect{width: 10, height: 5}
+	fmt.Println(r)
 
-   fmt.Println("area: ", r.area())
-   fmt.Println(r.x)
-   fmt.Println("perim: ", r.perim())
-   fmt.Println(r.x)
+	fmt.Println("area: ", r.area())
+	fmt.Println(r.x)
+	fmt.Println("perim: ", r.perim())
+	fmt.Println(r.x)
 
-   rp := &r
-   fmt.Println("area: ", rp.area())
-   fmt.Println("perim: ", rp.perim())
+	rp := &r
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim: ", rp.perim())
 }
